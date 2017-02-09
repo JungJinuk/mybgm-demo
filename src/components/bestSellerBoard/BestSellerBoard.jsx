6c7d3915d156378bf2b41936c09719069d890b0f
HeeYong Jang <heeyong@HeeYongui-MacBook-Pro.local>
2017-02-09 19:59:57 +0900

    bestSeller

---
+++ src/components/bestSellerBoard/BestSellerBoard.jsx
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react';
+import BestSeller from './BestSeller'
+import BestSellerCSS from '../../bestSeller.css'
+
+let bestSellers = [
+  { Title: 'City Of Stars', Artist: 'Ryan Gosling', Path: '/data/images/cover/City Of Stars.png', 
+    Wave: '/data/images/wave/City Of Stars_wave.png', Price: '1900', Song:'/data/musics/Imagine Dragons.mp3' },
+  { Title: 'Counting Stars', Artist: 'One Republic', Path: '/data/images/cover/Counting Stars.png', 
+    Wave: '/data/images/wave/Counting Stars_wave.png', Price: '1900', Song:'' },
+  { Title: 'Maps', Artist: 'Maroon5', Path: '/data/images/cover/Maps.png', 
+    Wave: '/data/images/wave/Maps_wave.png', Price: '1700', Song:'' },
+  { Title: 'Uptown Funk', Artist: 'Mark Ranson', Path: '/data/images/cover/Uptown Funk.png', 
+    Wave: '/data/images/wave/Uptown Funk_wave.png', Price: '1700', Song:'' },
+  { Title: 'Plug In Baby', Artist: 'Muse', Path: '/data/images/cover/Plug In Baby.png', 
+    Wave: '/data/images/wave/Plug In Baby_wave.png', Price: '1500', Song:'' },
+  { Title: 'Ai Se Eu Te Pego', Artist: 'Nossa Nossa', Path: '/data/images/cover/Ai Se Eu Te Pego.png', 
+    Wave: '/data/images/wave/Ai Se Eu Te Pego_wave.png', Price: '1500', Song:'' },
+];
+
+class BestSellerBoard extends Component {
+  render() {
+    const bestSellerDetail = bestSellers.map((bestSeller, index) => {
+      return (
+        <BestSeller
+          //onClick={() => this.handleClick(index)}
+          key={index}
+          bestSeller= {bestSeller}
+          style={BestSellerCSS}
+          />
+      )
+    });
+    return (
+      <div>
+        <ul>
+          {bestSellerDetail}
+        </ul>
+      </div>
+    );
+  }
+}
+BestSellerBoard.propTypes = {
+};
+
+export default BestSellerBoard;
--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import SearchBoard from '../../components/searchBar/SearchBoard'
 import mainCSS from '../../style_main.css'
 import CategoryBoard from '../../components/categoryBoard/CategoryBoard';
+import BestSellerBoard from '../../components/bestSellerBoard/BestSellerBoard'
 
 class MainPage extends Component {
   render() {
@@ -9,6 +10,7 @@ class MainPage extends Component {
       <div>
         <SearchBoard style={mainCSS}/>
         <CategoryBoard />
+        <BestSellerBoard/>
       </div>
     )
   }
