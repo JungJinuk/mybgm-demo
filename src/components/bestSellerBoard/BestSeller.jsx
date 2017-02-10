8d1661c168e4ca06f2b676a5e3aacd664c676d81
HeeYong Jang <heeyong@HeeYongui-MacBook-Pro.local>
2017-02-10 20:58:02 +0900

    react-wavesurfer add

--- src/components/bestSellerBoard/BestSeller.jsx
+++ src/components/bestSellerBoard/BestSeller.jsx
@@ -1,33 +1,32 @@
 import React, { Component, PropTypes } from 'react';
-
+import WaveController from './WaveController'
 
 
 class BestSeller extends Component {
-  play(){
-      //Sets the active song to the song being played. All other functions depend on this.
-      activeSong = document.getElementById("song");
-      //Plays the song defined in the audio tag.
-      activeSong.play();
-
-      //Calculates the starting volume percentage of the song.
-      //var percentageOfVolume = activeSong.volume / 1;
-      //var percentageOfVolumeMeter = document.getElementById('volumeMeter').offsetWidth * percentageOfVolume;
-
-      //Fills out the volume status bar.
-      //document.getElementById('volumeStatus').style.width = Math.round(percentageOfVolumeSlider) + "px";
+  constructor(props) {
+    super(props);
+    this.state ={
+      playing : false
     }
+    this.handleTogglePlay=this.handleTogglePlay.bind(this)
+  }
+  handleTogglePlay(){
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
   render() {
     
     let bestSeller = this.props.bestSeller;
 
     return (
       <li className="BestSeller">
-        <img className="play" src="/data/images/etc/play-button.png" onClick={this.props.play}/>
+        <img className="play" src={!this.state.playing ? "/data/images/etc/play-button.png":"/data/images/etc/pause-button.png"} 
+        onClick={this.handleTogglePlay}/>
         <img className="cover" src={bestSeller.Path} />
         <div className="detail"><p><b>{bestSeller.Title}</b></p>
           <p>{bestSeller.Artist}</p></div>
-        <img className="wave" src={bestSeller.Wave} />
-        <audio id="song" className="song" src={bestSeller.Song} controls />
+        <WaveController song={bestSeller.Song} playing={this.state.playing}/>
       </li>
     );
   }
