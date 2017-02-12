d400908dd01a9888b94efbdabc4e00f92a7a40df
JungJinuk <jinwook4217@daum.net>
2017-02-12 20:32:54 +0900

    feat : search keyword

--- src/containers/searchSound/SearchBar.jsx
+++ src/containers/searchSound/SearchBar.jsx
@@ -1,17 +1,52 @@
 import React, { Component, PropTypes } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      searchText: e.target.value
+    });      
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onUserInput(this.state.searchText);
+    this.setState({
+      searchText: ''
+    });
+  }
+
   render() {
     return (
-      <div classID="search-input">
+      <div className="search-input">
         <div className="container">
           <div className="row">
-            <div className="col-md-3 col-sm-6">
-              <input 
-                type="text" 
-                placeholder="What are you looking for? (ex: happy, rock, nature...)"
-              />
-            </div>
+            <form onSubmit={this.handleSubmit}>
+              <div className="input-group">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="What are you looking for? (ex: happy, rock, nature...)"
+                  onChange={this.handleChange}
+                  value={this.state.searchText}
+                />
+                <span className="input-group-btn">
+                  <button className="btn btn-default">
+                    <span className="glyphicon glyphicon-search"></span>
+                  </button>
+                </span>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -20,7 +55,7 @@ class SearchBar extends Component {
 }
 
 SearchBar.propTypes = {
-
+  onUserInput: PropTypes.func
 };
 
 export default SearchBar;
--- src/containers/searchSound/SearchSound.jsx
+++ src/containers/searchSound/SearchSound.jsx
@@ -1,13 +1,48 @@
 import React, { Component, PropTypes } from 'react';
+
 import NaviBar from '../../components/NaviBar/NaviBar';
 import Footer from '../../components/Footer/Footer';
+import SearchBar from './SearchBar';
+import SearchKeywordBoard from './SearchKeywordBoard';
+import BestSellerBoard from '../../components/bestSellerBoard/BestSellerBoard';
+
+import searchBarCSS from './search-bar.css';
+import searchKeywordBoardCSS from './search-keyword.css'
 
 class SearchSound extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      searchKeywords:[]
+    }
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(searchText) {
+    this.setState((prevState) => {
+      var localSearchText = searchText; 
+      return {
+        searchKeywords: prevState.searchKeywords.concat(localSearchText)
+      };
+    });
+    console.log(this.state.searchKeywords)
+  }
+  
   render() {
     return (
       <div>
         <NaviBar/>
-        <h1>SearchSound</h1>
+        <SearchBar 
+          style={searchBarCSS}
+          onUserInput={this.handleUserInput}
+        />
+        <SearchKeywordBoard 
+          style={searchKeywordBoardCSS}
+          searchKeywords={this.state.searchKeywords}  
+        />
+        <BestSellerBoard />
         <Footer/>
       </div>
     );
